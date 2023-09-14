2959c2c6bd95ad2c56d90f4721f3be5b3b9fbf92
lilystrus <gakr4055@colorado.edu>
2023-09-14 01:06:31 +0000

    print_fires.py has been modified to find Agrofood_co2_emission.csv in the current working directory and print out the total number of (forest) fires in the U.S. across all the years documented in the cvs file.

--- print_fires.py
+++ print_fires.py
@@ -1,6 +1,20 @@
 country='United States of America'
-county_column = 1
+country_column = 1
+# for Savanna files use 3, for Forest fires use 4 (2 and 3 respectively when passing to get_column() )
+# here assume fires are Forest fires 
 fires_column = 4
-file_name = 'Agrofood_co2_emission.csv'
-fires = get_column()
-print(fires)
+
+# get current working directory; assumes the Agro file is in there 
+import os 
+cwd = os.getcwd()
+file_name = cwd+'/Agrofood_co2_emission.csv'
+
+# import the module containing get_column() and call it for parameters set above 
+import my_utils as mu
+fires = mu.get_column(file_name,country_column-1,country,fires_column-1)
+
+# convert the string list 'fires' into a float list 'fires_num'
+# add all elements of 'fires_num' to get total number of fires for all years documented in the Agro file
+fires_num = [ float(i) for i in fires ] 
+fires_sum = sum(fires_num)
+print('The total number of (forest) fires in '+country+' is '+str(fires_sum))
