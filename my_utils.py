cc020c606f5bb0f69aa170c2a429b14165eeeaf8
lilystrus <gakr4055@colorado.edu>
2023-09-14 03:02:48 +0000

    corrected the passed column indices; now user gives column number, not col.num.-1=index into array

--- my_utils.py
+++ my_utils.py
@@ -5,8 +5,8 @@ def get_column(file_name, query_column, query_value, result_column):
     file = open(file_name,'r')
     for line in file:
         ListofLine = line.strip().split(',')
-        if ListofLine[query_column] == query_value:
-            ResultList.append(ListofLine[result_column])
+        if ListofLine[query_column-1] == query_value:
+            ResultList.append(ListofLine[result_column-1])
 
     file.close()
    
